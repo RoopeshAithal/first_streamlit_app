b8a72e23f288b34fc8b0b4e1db9f879703a4bd6d
RoopeshAithal <RoopeshAithal@gmail.com>
2022-05-21 08:01:08 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 
 import streamlit
 streamlit.title('My new streamlit app')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Omlett')
+streamlit.text('Blueberry , Spinach & dates Smoothie')
+streamlit.text('Boiled Egg')
