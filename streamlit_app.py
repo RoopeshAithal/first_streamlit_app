0d1ebc5a48c1780fa6d6f21f386f17c3b04d6506
RoopeshAithal <RoopeshAithal@gmail.com>
2023-04-25 07:55:16 -0400

    removed
    new connections

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,12 +56,7 @@ def insert_row_snowflake(new_fruit):
 		my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
 		return "Thanks for adding "+new_fruit
 	
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+
 
 add_my_fruit = streamlit.text_input('What fruit would you like add?')
 if streamlit.button('Add a Fruit to the List'):
